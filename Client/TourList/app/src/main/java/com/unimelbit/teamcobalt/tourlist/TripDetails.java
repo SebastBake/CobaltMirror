d5fd728626e31445526a6830ab31592724827851
Sebastian Baker <bakersebastian00@gmail.com>
2017-08-30 08:43:02 +1000

    Foundation activities created

---
+++ Client/TourList/app/src/main/java/com/unimelbit/teamcobalt/tourlist/TripDetails.java
@@ -0,0 +1,13 @@
+package com.unimelbit.teamcobalt.tourlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_details);
+    }
+}
