d01f2f22848ee26f5af9799ac19eb5b2737e7260
honglin96 <30568308+honglin96@users.noreply.github.com>
2017-09-26 21:02:13 +1000

    Comments for Chat and factory

--- Client/TourList/app/src/main/java/com/unimelbit/teamcobalt/tourlist/AppServicesFactory.java
+++ Client/TourList/app/src/main/java/com/unimelbit/teamcobalt/tourlist/AppServicesFactory.java
@@ -9,12 +9,19 @@ import com.unimelbit.teamcobalt.tourlist.Chat.FirebaseChatRoomHandler;
  * Created by Hong Lin on 26/09/2017.
  */
 
+/**
+ * Singleton factory which creates relevant handlers and classes to be used in the app
+ */
 public class AppServicesFactory {
 
     private static AppServicesFactory servicesInstance;
 
     private ChatAdaptor firebaseChatService;
 
+    /**
+     * Return itself as it is a singleton class
+     * @return
+     */
     public static AppServicesFactory getServicesFactory(){
 
         if(servicesInstance == null){
@@ -28,6 +35,11 @@ public class AppServicesFactory {
     }
 
 
+    /**
+     * Returns the Firebase chat handler
+     * @param c
+     * @return
+     */
     public ChatAdaptor getFirebaseChatService(Context c){
 
         if(firebaseChatService == null){
--- Client/TourList/app/src/main/java/com/unimelbit/teamcobalt/tourlist/Chat/ChatAdaptor.java
+++ Client/TourList/app/src/main/java/com/unimelbit/teamcobalt/tourlist/Chat/ChatAdaptor.java
@@ -4,14 +4,19 @@ package com.unimelbit.teamcobalt.tourlist.Chat;
  * Created by Hong Lin on 26/09/2017.
  */
 
+/**
+ * Adaptor interface for chat used by this app
+ */
 public interface ChatAdaptor {
 
+    //Generates chat room
+    void generateChatRoom(String name);
 
-    public void generateChatRoom(String name);
+    //Takes uer into the chat room
+    void enterChatRoom(String userName, String room);
 
-    public void enterChatRoom(String userName, String room);
-
-    public void checkRoom(String room);
+    //Checks if the room is present in DB
+    void checkRoom(String room);
 
 
 }
