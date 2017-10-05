a9f0f6e9976232c2860a5339d9b2875963825522
Alexander White <awhite@192-168-1-7.tpgi.com.au>
2017-10-06 00:35:13 +1100

    Basic tests added for AR

--- Client/TourList/app/src/test/java/com/unimelbit/teamcobalt/tourlist/AugmentedReality/ARJSONListenerTest.java
+++ Client/TourList/app/src/test/java/com/unimelbit/teamcobalt/tourlist/AugmentedReality/ARJSONListenerTest.java
@@ -1,8 +1,43 @@
-import static org.junit.Assert.*;
+package com.unimelbit.teamcobalt.tourlist.AugmentedReality;
+
+import android.content.Context;
+
+import com.wikitude.architect.ArchitectJavaScriptInterfaceListener;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by awhite on 5/10/17.
  */
 public class ARJSONListenerTest {
+
+    ARJSONListener listener;
+
+    @Before
+    public void setUp() {
+        this.listener = new ARJSONListener();
+    }
+
+    @Test
+    public void getArchitectJavaScriptInterfaceListener() throws Exception {
+        Context context = mock(Context.class);
+        ArchitectJavaScriptInterfaceListener temp = listener.getArchitectJavaScriptInterfaceListener(context);
+        assertNotNull(temp);
+
+    }
+
+    @Test
+    public void createListener() throws Exception {
+
+    }
+
+    @Test
+    public void setListener() throws Exception {
+
+    }
 
 }
