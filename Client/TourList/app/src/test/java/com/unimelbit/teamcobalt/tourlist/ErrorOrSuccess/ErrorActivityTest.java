68f6305773dfb7ed15f7cf4b38c48255b2db77fc
AlexWhiteAU <awhite5@student.unimelb.edu.au>
2017-10-23 00:17:50 +1100

    Comments added to ErrorOrSuccess Tests

--- Client/TourList/app/src/test/java/com/unimelbit/teamcobalt/tourlist/ErrorOrSuccess/ErrorActivityTest.java
+++ Client/TourList/app/src/test/java/com/unimelbit/teamcobalt/tourlist/ErrorOrSuccess/ErrorActivityTest.java
@@ -5,19 +5,23 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+
 /**
- * Created by awhite on 17/10/17.
+ * Check androidTest folder for tests of ErrorAcitivty.
+ * The class has no application logic to test, it creates an
+ * Activity and View to display the error messages.
  */
+
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest=Config.NONE)
 public class ErrorActivityTest {
 
-    /**
-     * Check androidTest folder for tests of ErrorAcitivty.
-     * The class has no application logic to test, it creates an
-     * Activity and View to display the error messages.
+    /*
+     * Place holder because class needs to contain a test
+     *
+     * Replace when/if tests are added.
+     * i.e. if application logic is added to the class.
      */
-
     @Test
     public void stubTest() throws Exception {
 
